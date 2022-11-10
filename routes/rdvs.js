36c4add4f3042843cbbb0f8c9e6c23bcce2c136c
Juliersh <55799409+Juliersh@users.noreply.github.com>
2022-11-10 11:55:52 +0100

    Update rdvs.js

--- routes/rdvs.js
+++ routes/rdvs.js
@@ -1,42 +1,65 @@
-
 import express from "express";
-import { RDV } from "../model/Rdv.js"
+import { RDV } from "../model/Rdv.js";
 const router = express.Router();
 
-//RDV
+//GET
+router.get("/", async (req, res, next) => {
 
-//  GET
-// /rdvs
-router.get("/", (req, res) => {
-    RDV.find().sort('titre').exec(function (err, theRdv) {
-        if (err) {
-            return next(err);
-        }
-        res.send(theRdv);
-    });
+    try {
+        const rdvs = await RDV.find({})
+        res.status(200)
+        res.send(rdvs);
+      } catch(e) {
+        res.send(e)
+      }
+
+
+
+    // RDV.find().sort('titre').exec(function (err, theRdv) {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     res.send(theRdv);
+    // });
 });
 
 
 //  POST 
 // /users
-router.post("/", (req, res) => {
+router.post("/", async (req, res, next) => {
     //générer l'ID tout seul?
-    const newRdv = new RDV({
-        RDV_ID: req.body.RDV_ID,
-        provider: req.body.provider,
-        reciever: req.body.reciever,
-        isAccepted: req.body.isAccepted
-    })
-    if (!newRdv.RDV_ID || !newRdv.provider || !newRdv.reciever) {
-        return res.sendStatus(400);
-    } else {
-        newRdv
-            .save()
-            .then(
-                () => console.log("One entry added"),
-                (err) => console.log(err)
-            );
-    }
+   
+    try {
+        const newRdv = await new RDV({
+            RDV_ID: req.body.RDV_ID,
+            provider: req.body.provider,
+            reciever: req.body.reciever,
+            isAccepted: req.body.isAccepted
+        })
+        .save()
+        res.status(200)
+        res.send(newRdv);
+      } catch(e) {
+        res.send(e)
+      }
+   
+
+    // const newRdv = new RDV({
+    //     RDV_ID: req.body.RDV_ID,
+    //     provider: req.body.provider,
+    //     reciever: req.body.reciever,
+    //     isAccepted: req.body.isAccepted
+    // })
+    // if (!newRdv.RDV_ID || !newRdv.provider || !newRdv.reciever) {
+    //     return res.sendStatus(400);
+    // } else {
+    //     newRdv
+    //         .save()
+    //         .then(
+    //             () => console.log("One entry added"),
+    //             (err) => console.log(err)
+    //         );
+    // }
 
 });
 
