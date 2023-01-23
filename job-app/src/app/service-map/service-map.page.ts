3eaf7ff95279e8619131bccf4b303eac63bde98b
Juliersh <55799409+Juliersh@users.noreply.github.com>
2023-01-23 12:57:30 +0100

    Ajustements

--- job-app/src/app/service-map/service-map.page.ts
+++ job-app/src/app/service-map/service-map.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ModalController, NavParams } from '@ionic/angular';
+import { CreateServicePage } from '../layout/create-service/create-service.page';
 
 @Component({
   selector: 'app-service-map',
@@ -8,13 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceMapPage implements OnInit {
   
+    services: any;
+    serviceApiUrl = '';
+    serviceData = {
+      _id: '',
+      titre:'',
+      type: '',
+      date: '',
+      provider: '',
+      location:'',
+      picture:'',
+    }
 
-
-  constructor() { 
+  constructor(public http: HttpClient, private modalController: ModalController) { 
    
+    this.readAPI('https://jobapp.onrender.com/services')
+    .subscribe((data) => {
+      this.services = data['data'];
+    });
+}
 
+readAPI(URL: string){
+  return this.http.get(URL)
+}
+
+async openCreateServiceModal() {
+  const modal = await this.modalController.create({
+      component: CreateServicePage,
+      componentProps: {
+        // pass any props that your create service component needs
+      },
+      cssClass: 'createServiceModal'
+  });
+  return await modal.present();
     
   }
+
+  async openServiceCard() {
+    const card = await this.modalController.create({
+        component: CreateServicePage,
+        componentProps: {
+          // data: service
+          // pass any props that your create service component needs
+        },
+        cssClass: 'createServiceModal'
+    });
+    return await card.present();
+      
+    }
+  
 
  
 
