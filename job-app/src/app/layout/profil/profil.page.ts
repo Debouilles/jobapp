1069984f51a9126a4c0b3b02b728f819b8abf70d
TanyaKemmPere <tanya.kemmpereira@heig-vd.ch>
2023-01-23 13:00:03 +0100

    affiche liste "mes annonces"
    mais ce n'est pas lié au current user

--- job-app/src/app/layout/profil/profil.page.ts
+++ job-app/src/app/layout/profil/profil.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from "src/app/auth/auth.service";
 import { Router } from "@angular/router";
 
+import { Service } from 'src/app/models/service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profil',
@@ -11,13 +13,35 @@ import { Router } from "@angular/router";
 export class ProfilPage implements OnInit {
 
   selectTabs = 'default';
+
+  services: any;
+  
+  serviceData = {
+    _id: '',
+    titre:'',
+    type: '',
+    date: '',
+    provider: '',
+    location:'',
+    picture:'',
+  }
   
   constructor(
+    public http: HttpClient,
     // Inject the authentication provider.
     private auth: AuthService,
     // Inject the router
     private router: Router
-  ) {}
+  ) {
+
+    this.readAPI('https://jobapp.onrender.com/services')
+      .subscribe((data) => {
+        this.services = data['data'];
+        return this.services;
+      });
+
+      console.log(this.services)
+  }
 
   ngOnInit() {
   }
@@ -26,4 +50,9 @@ export class ProfilPage implements OnInit {
     this.auth.logOut();
     this.router.navigateByUrl("/login");
   }
+  
+  readAPI(URL: string){
+    return this.http.get(URL)
+  }
+
 }
