bf01f2b26355c8882e97bd00e70492b625754f6a
dk-xy <92148260+dk-xy@users.noreply.github.com>
2023-01-28 15:44:30 +0100

    feedback creation user

--- job-app/src/app/layout/registrate/registrate.page.ts
+++ job-app/src/app/layout/registrate/registrate.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from '../services/user.service';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-registrate',
   templateUrl: './registrate.page.html',
@@ -15,18 +18,52 @@ export class RegistratePage implements OnInit {
 
   }
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, public Router: Router, public toast: ToastController) {
 
   }
+
+
+
+  async createdUserMessage() {
+    const toast = await this.toast.create({
+      message: 'Compte créé, veuillez vous <b>Connecter</b>',
+      duration: 1500,
+      position: 'middle',
+      color: 'success',
+      cssClass: 'sucess-toaster'
+    });
+
+    await toast.present();
+  }
+
+
+  async errorMessage() {
+    const toast = await this.toast.create({
+      message: 'une Erreur est survenue',
+      duration: 1500,
+      position: 'middle',
+      color: 'danger',
+      cssClass: 'sucess-toaster'
+    });
+
+    await toast.present();
+  }
+
+
+
+
 
   onSubmit(form: NgForm) {
     const { name, email, password } = form.value;
     this.userService.createUser(name, email, password).subscribe((response) => {
       console.log(response);
+      this.createdUserMessage();
+      this.Router.navigate(['/login']);
 
     },
       (error) => {
         console.error(error);
+        this.errorMessage();
       }
 
     );
